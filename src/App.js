4b0afa1d2f6e7e33c0efe049b3d67e0f92c93f46
Kukjin Kim <tlaruddo2@gmail.com>
2022-01-19 13:15:57 -0800

    Create Customer.jsx

--- src/App.js
+++ src/App.js
@@ -1,17 +1,59 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer';
+
+const customers =[
+  {
+  "id":1,
+  "image": "https://placeimg.com/64/64/1",
+  "name" : 'jin',
+  "birthday":"951212",
+  "gender":"male",
+  "job":"student"
+},
+  {
+  "id":2,
+  "image": "https://placeimg.com/64/64/2",
+  "name" : 'james',
+  "birthday":"951212",
+  "gender":"male",
+  "job":"student"
+},
+  {
+  "id":3,
+  "image": "https://placeimg.com/64/64/3",
+  "name" : 'kfje',
+  "birthday":"951212",
+  "gender":"male",
+  "job":"student"
+},
+]
+
 
 class App extends Component {
-  state = {  } 
+
   render() { 
     return (
-      <div className='gray-background'>
-        <img src={logo} lat="logo" />
-        <h2>gogo</h2>
+      //jsx는 렌더하기전 요소가 여러개면 무조건 div같은 요소로 전체를 감싸야 한다 
+      <div>
+        {/* map은 key component 필수  */}
+        {customers.map( c =>{
+          return(
+            <Customer
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+            />
+          );
+        })}
       </div>
+      
     );
   }
 }
- 
+
 export default App;
