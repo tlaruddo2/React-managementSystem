77e90558d1ef97b9f2fb3d532e29f2bd1e3d7e6f
Kukjin Kim <tlaruddo2@gmail.com>
2022-01-20 00:36:56 -0800

    Add get customers REST API Module

--- client/src/App.js
+++ client/src/App.js
@@ -20,42 +20,41 @@ const styles =  theme => ({
   table:{
     minWidth: 1080
   }
-})
-
-const customers =[
-  {
-  "id":1,
-  "image": "https://placeimg.com/64/64/1",
-  "name" : 'jin',
-  "birthday":"951212",
-  "gender":"male",
-  "job":"student"
-},
-  {
-  "id":2,
-  "image": "https://placeimg.com/64/64/2",
-  "name" : 'james',
-  "birthday":"951212",
-  "gender":"male",
-  "job":"student"
-},
-  {
-  "id":3,
-  "image": "https://placeimg.com/64/64/3",
-  "name" : 'kfje',
-  "birthday":"951212",
-  "gender":"male",
-  "job":"student"
-},
-]
+});
 
 
 class App extends Component {
+  //props: 변경될수 없는 데이터 , state: 변결할 수 있는 데이터 
+  state = {
+    customers:""
+  }
+
+  //api에 접근해서 data받아오는경우 
+  //생명주기가 존재
+  //모든 component가 mount가 된 후 실행 
+  componentDidMount(){
+    //body가 res로
+    //then: 받아서 설정 
+    //catch: 에러 핸들
+    this.callApi()
+      .then(res => this.setState({customers:res}))
+      .catch(err => console.log(err)); 
+  }
+  
+
+  //비동기적으로 : 처음에느 리액트 컴포넌트 구성되면 state값이 처음에 비어있다 
+  //약간 시간이 지나야 채워진다 그래서 존재하지 않는 경우도 다르게 설정
+  callApi = async() => {
+    //response: 접속하고자하는 api주소 
+    //고객목록을 body변수에 담기
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
 
   render() { 
     const{classes} = this.props
     return (
-      //jsx는 렌더하기전 요소가 여러개면 무조건 div같은 요소로 전체를 감싸야 한다 
       <Paper className={classes.root}>
         <Table className={classes.table}>
           <TableHead>
@@ -69,7 +68,9 @@ class App extends Component {
             </TableRow>
           </TableHead>
           <TableBody>
-            {customers.map( c => { return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender}job={c.job}/>);})}
+            {/* 존재하지않는경우도 설정 */}
+            {this.state.customers ? this.state.customers.map( c => { return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender}job={c.job}/>);})
+            :""}
           </TableBody>
         </Table>
       </Paper>
--- server.js
+++ server.js
@@ -6,8 +6,34 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : true}));
 
-app.get('/api/hello', (req,res) => {
-    res.send({message:"hello express"});
+//rest api
+app.get('/api/customers', (req,res) => {
+    res.send([
+                {
+        "id":1,
+        "image": "https://placeimg.com/64/64/1",
+        "name" : "jin",
+        "birthday":"951212",
+        "gender":"male",
+        "job":"student"
+        },
+        {
+        "id":2,
+        "image": "https://placeimg.com/64/64/2",
+        "name" : "james",
+        "birthday":"951212",
+        "gender":"male",
+        "job":"student"
+        },
+        {
+        "id":3,
+        "image": "https://placeimg.com/64/64/3",
+        "name" : "kfje",
+        "birthday":"951212",
+        "gender":"male",
+        "job":"student"
+        }
+    ]);
 });
 
 
