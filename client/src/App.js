ddf4792d0a412a7c378017c7e01ee6e9ba811bb5
Kukjin Kim <tlaruddo2@gmail.com>
2022-01-31 02:11:14 -0800

    Add material ui app bar with search

--- client/src/App.js
+++ client/src/App.js
@@ -11,21 +11,93 @@ import { withStyles } from '@material-ui/styles'; //apply scc to material-us
 import { Paper } from '@material-ui/core';  //wrap outside of components
 import { CircularProgress } from '@material-ui/core';
 
-const styles =  theme => ({
-  // findout how to use theme here?
-  root:{
-    width: '100%',
-    // marginTop: theme.spacing.unit *3, 
-    // marginTop: theme.spacing(3),
-    overflowX: "auto"
-  },
-  table:{
-    minWidth: 1080
-  },
-  progress:{
-    // margin:
-  }
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import InputBase from '@material-ui/core/InputBase';
+import { fade } from '@material-ui/core/styles/colorManipulator';
+import Typography from '@material-ui/core/Typography';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+
+
+const styles = theme => ({
+root: {
+width: "100%",
+minWidth: 1080
+},
+menu: {
+marginTop: 15,
+marginBottom: 15,
+display: 'flex',
+justifyContent: 'center'
+},
+paper: {
+marginLeft: 18,
+marginRight: 18
+},
+progress: {
+margin: theme.spacing.unit * 2
+},
+grow: {
+flexGrow: 1,
+},
+tableHead: {
+fontSize: '1.0rem'
+},
+menuButton: {
+marginLeft: -12,
+marginRight: 20,
+},
+title: {
+display: 'none',
+[theme.breakpoints.up('sm')]: {
+display: 'block',
+},
+},
+search: {
+position: 'relative',
+borderRadius: theme.shape.borderRadius,
+backgroundColor: fade(theme.palette.common.white, 0.15),
+'&:hover': {
+backgroundColor: fade(theme.palette.common.white, 0.25),
+},
+marginLeft: 0,
+width: '100%',
+[theme.breakpoints.up('sm')]: {
+marginLeft: theme.spacing.unit,
+width: 'auto',
+},
+},
+searchIcon: {
+width: theme.spacing.unit * 9,
+height: '100%',
+position: 'absolute',
+pointerEvents: 'none',
+display: 'flex',
+alignItems: 'center',
+justifyContent: 'center',
+},
+inputRoot: {
+color: 'inherit',
+width: '100%',
+},
+inputInput: {
+paddingTop: theme.spacing.unit,
+paddingRight: theme.spacing.unit,
+paddingBottom: theme.spacing.unit,
+paddingLeft: theme.spacing.unit * 10,
+transition: theme.transitions.create('width'),
+width: '100%',
+[theme.breakpoints.up('sm')]: {
+width: 120,
+'&:focus': {
+width: 200,
+},
+},
+}
 });
+
 
 
 class App extends Component {
@@ -37,9 +109,6 @@ class App extends Component {
     }
   }
 
-  //this function is called from customerAdd component 
-  //which referesh only table, not whole page
-  //pass this function to customerAdd component as props
   stateRefresh = () => {
     this.setState({
       customers: '',
@@ -71,19 +140,42 @@ class App extends Component {
 
   render() { 
     const{classes} = this.props
+    const cellList = ["No.", "Profile","Name","Birthday","Gender","Job","Setting"]
     return (
-      <div>
-          <Paper className={classes.root}>
+      <div className={classes.root}>
+          <AppBar position="static">
+          <Toolbar>
+          <IconButton className={classes.menuButton} color="inherit" aria-label="Open drawer">
+          <MenuIcon />
+          </IconButton>
+          <Typography className={classes.title} variant="h6" color="inherit" noWrap>
+          Customer Management System
+          </Typography>
+          <div className={classes.grow} />
+          <div className={classes.search}>
+          <div className={classes.searchIcon}>
+          <SearchIcon />
+          </div>
+          <InputBase
+          placeholder="Search"
+          classes={{
+          root: classes.inputRoot,
+          input: classes.inputInput,
+          }}
+          />
+          </div>
+          </Toolbar>
+          </AppBar>
+          <div className = {classes.menu}>
+            <CustomerAdd stateRefresh={this.stateRefresh}/>
+          </div>
+          <Paper claaName = {classes.paper}>
           <Table className={classes.table}>
             <TableHead>
               <TableRow>
-                <TableCell>No.</TableCell>
-                <TableCell>Photo</TableCell>
-                <TableCell>Name</TableCell>
-                <TableCell>Birthday</TableCell>
-                <TableCell>Gender</TableCell>
-                <TableCell>Job</TableCell>
-                <TableCell>Delete</TableCell>
+                {cellList.map( c => {
+                  return <TableCell className = {classes.tableHead}>{c}</TableCell>
+                })}
               </TableRow>
             </TableHead>
             <TableBody>
@@ -98,7 +190,7 @@ class App extends Component {
             </TableBody>
           </Table>
         </Paper>
-        <CustomerAdd stateRefresh={this.stateRefresh}/>
+        
       </div>
       
       
--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ThemeProvider } from '@material-ui/styles';
+
+import { createTheme } from '@material-ui/core/styles';
+const theme = createTheme({
+});
 
 ReactDOM.render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
+  // document.getElementById('root')
+
+  <ThemeProvider theme={theme}>
     <App />
-  </React.StrictMode>,
+  </ThemeProvider>,
   document.getElementById('root')
 );
 
